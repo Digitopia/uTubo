7459e4cba26020ea9b6e5d78f36aba787aa6b3b9
p1nh0 <p1nh0.c0d1ng@gmail.com>
2015-02-13 16:03:21 +0000

    Uploading Mozzi library
    This library (version 0.01.1s) had minor changes to work with my
    Arduino Mega 2560 board

---
+++ Arduino/Mozzi/doc/html/search/functions_67.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['getsensor',['getSensor',['../group__analog.html#ga720bf7cf527ba071bfe2e4263f16fa9d',1,'getSensor(unsigned char channel_num):&#160;mozzi_analog.cpp'],['../group__analog.html#ga720bf7cf527ba071bfe2e4263f16fa9d',1,'getSensor(unsigned char channel_num):&#160;mozzi_analog.cpp']]]
+];
